461443d2de204276a778654e740f2cf08e9bf2b8
DmitriyD221 <143995912+DmitriyD221@users.noreply.github.com>
2023-09-05 13:13:59 +0300

    Add files via upload

---
+++ oopdz1/oopdz1/oopdz1.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+using namespace std;
+
+//CLASS 1
+/*
+class human {
+public:
+	double heigh = 1.8; // meters
+	int arms_count = 2;
+	string hair_color = "brown";
+	string name = "Jacob";
+	string skin_color = "white";
+
+	void EatSomething() {
+		cout << "Hotdog?\n";
+	}
+	void GoSomewhere() {
+		cout << "Theatre?\n";
+	}
+	void DoSomething() {
+		cout << "Sleep?\n";
+	}
+	void TalkToSomeone() {
+		cout << "Friends?\n";
+	}
+	void DrinkSomething() {
+		cout << "Water?\n";
+	}
+};
+
+int main()
+{
+}
+*/
+
+
+//CLASS 2
+/*
+class room {
+public:
+	double ceilingsHeigh = 3.5; // meters
+	int seats_count = 5;
+	int windows_count = 1;
+	string walls_color = "white";
+	string room_type = "kitchen";
+
+	void Furniture1() {
+		cout << "Oven\n";
+	}
+	void Furniture2() {
+		cout << "Dining Table\n";
+	}
+	void Furniture3() {
+		cout << "Wooden Chairs\n";
+	}
+	void Furniture4() {
+		cout << "Shelfs\n";
+	}
+	void Furniture5() {
+		cout << "Washing Machine\n";
+	}
+};
+
+int main()
+{
+}
+*/
+
+
+//CLASS 3
+/*
+class phone {
+public:
+	int memory = 256gb; // meters
+	double phone_heigh = 5.78; //inches
+	double phone_width = 2.81; //inches
+	string phone_color = "mate_black";
+	string phone_brand = "iphone";
+
+	void Store() {
+		cout << "Apple Store\n";
+	}
+	void PhoneApp1() {
+		cout << "Camera\n";
+	}
+	void PhoneApp2() {
+		cout << "Google Maps\n";
+	}
+	void PhoneApp3() {
+		cout << "Calculator\n";
+	}
+	void PhoneApp4() {
+		cout << "Calendar\n";
+	}
+};
+
+int main()
+{
+}
+*/
+
+
+//CLASS 4
+/*
+class car {
+public:
+	int car_price = 25000; //dollars
+	int wheels_count = 4; 
+	double car_lengh = 4.8; //meters
+	string car_color = "white";
+	string car_type = "coupet";
+
+	void SeatsMaterial() {
+		cout << "Leather\n";
+	}
+	void TyresCondition() {
+		cout << "Brand New\n";
+	}
+	void TyresSeason() {
+		cout << "Summer\n";
+	}
+	void CarAccidents() {
+		cout << "None\n";
+	}
+	void CarBrand() {
+		cout << "Chevrolet\n";
+	}
+};
+
+int main()
+{
+}
+*/
+
+//CLASS 5
+
+class Forest {
+public:
+	double trees_count = 6.2; //millions
+	int mountains_count = 2;
+	double forest_size = 65.2; // sq. kilometers
+	string forest_season = "fall";
+	string region = "appalachia"; //region in the us
+
+	void MainTreeType() {
+		cout << "Tulip Tree\n";
+	}
+	void BiggestRiver() {
+		cout << "Susquehanna River\n";
+	}
+	void BestSeasonToVisit() {
+		cout << "Fall\n";
+	}
+	void AppalachiaRegionPopulation() {
+		cout << "26.3 million people\n";
+	}
+	void AppalachiaRegionSize() {
+		cout << "1.909 million square kilometers\n";
+	}
+};
+
+int main()
+{
+}
